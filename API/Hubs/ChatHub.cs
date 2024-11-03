defe4f7241012949500aeec55221180443f37b2c
Dang Van Hai <vanhaicntt5@gmail.com>
2024-11-03 19:58:51 +0700

    fix: ...

--- API/Hubs/ChatHub.cs
+++ API/Hubs/ChatHub.cs
@@ -4,8 +4,15 @@ namespace API.Hubs;
 
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public override async Task OnConnectedAsync()
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+        await base.OnDisconnectedAsync(exception);
     }
 }
